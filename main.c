9cc9e00a91f5629d4e6ae49556b4f592d73e6213
Gino <gminnucci@itba.edu.ar>
2019-10-20 22:22:00 -0300

    esto

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: gino
+ *
+ * Created on October 20, 2019, 10:20 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
